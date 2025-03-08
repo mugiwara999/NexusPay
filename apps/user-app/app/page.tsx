bfd7337b2fc6458a0f859fc86a3c39c3483f1e69
mugiwara999 <125131353+mugiwara999@users.noreply.github.com>
2025-03-08 12:31:01 +0530

    prisma init

--- apps/user-app/app/page.tsx
+++ apps/user-app/app/page.tsx
@@ -1,8 +1,11 @@
+import { PrismaClient } from "@repo/db/client"
 
 export default function Home() {
   return (
-    <h1 className="text-3xl font-bold underline">
+    <h1 className="hover:text-3xl not-hover::text-xl font-bold underline">
       Hi from tailwind
+
     </h1>
+
   )
 }
